bd3db97d79142e8920fad34580fbceef52a132d9
Martin Sosa <62191219+Zuquii@users.noreply.github.com>
2025-04-28 14:31:39 -0300

    Create Resultados.java
    Actividad1.

---
+++ Resultados.java
@@ -0,0 +1,17 @@
+/*Indique el resultado que se obtiene de las siguientes
+expresiones en Java:
+ • boolean resultado = (10 > 5) && (3 < 2) || (4 == 4);
+ • int y = (50 / 5) + (3 * 2) - 1;
+*/
+
+
+class Resultados{
+ 
+ public static void main(String args[]) { 
+    boolean resultado = (10 > 5) && (3 < 2) || (4 == 4);
+	int y = (50/5) + (3*2) - 1;
+	
+	System.out.println("Resultado = " + resultado);
+	System.out.println("Y = " + y);
+    }
+}	
